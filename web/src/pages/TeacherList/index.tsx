a70c06cb18e7e4fd3ac2ea3b96dd7d133d324bc7
Walicen Dalazuana <walicen.r@gmail.com>
2020-08-07 17:14:13 -0300

    finish Teache Form

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,28 +2,41 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
+
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Select name="subject" label="Matéria" options={[
+            { value: 'Artes', label: 'Artes' },
+            { value: 'Biologia', label: 'Biologia' },
+            { value: 'Educação Física', label: 'Educação Física' },
+            { value: 'Português', label: 'Português' },
+            { value: 'Matemática', label: 'Matemática' },
+            { value: 'Quimica', label: 'Quimica' },
+            { value: 'Sociologia', label: 'Sociologia' },
+            { value: 'Filosofia', label: 'Filosofia' },
+          ]} />
+          <Select name="week_day" label="Dia da semana" options={[
+            { value: '0', label: 'Domingo' },
+            { value: '1', label: 'Segunda-feira' },
+            { value: '2', label: 'Terça-feira' },
+            { value: '3', label: 'Quarta-feira' },
+            { value: '4', label: 'Quinta-feira' },
+            { value: '5', label: 'Sexta-feira' },
+            { value: '6', label: 'Sábado' },
+          ]} />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="time" label="Hora" type="time" />
+
+
         </form>
       </PageHeader>
 
